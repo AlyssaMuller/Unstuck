1d5d7ab07a7a11094d854ba087ffb2522d2cd5bb
Alyssa <alyssanm18@gmail.com>
2022-01-13 22:08:59 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("8BallAssignment");
+
+var x=5;
+
+var h1=document.querySelector('h1');
+console.log("h1 query:", h1);
+h1.style.color='#0000CC';
+h1.innerHTML="Nah";
+
+var myButton=document.querySelector("#mainButton");
+
+console.log("button query:", mainButton);
